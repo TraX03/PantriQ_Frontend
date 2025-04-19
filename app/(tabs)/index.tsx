4342c430aaaa5ccfc884b6401e1c358b80f58f3c
traZe <xiweilim823@gmail.com>
2025-04-19 12:32:50 +0800

    Set up Reactotron, remove package-lock.json and update .gitignore

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import HomeView from "@/app/Home/component";
 
+if (__DEV__) {
+  require("@/ReactotronConfig");
+}
+
 export default function HomePage() {
   return <HomeView />;
 };
